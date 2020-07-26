5447bb72fb038ef63d84749034afe41ba1394422
Ville Manninen <vw.manninen@gmail.com>
2020-07-26 22:29:51 +0300

    added CSS folder

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request
 
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
 
 import routes
