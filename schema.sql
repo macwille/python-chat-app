fa257aceb96c3d78142b91481c8dc0e733d3e75c
Ville Manninen <vw.manninen@gmail.com>
2020-07-31 19:35:26 +0300

    modified schema

---
+++ schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    name varchar(21) NOT NULL,
+    password TEXT NOT NULL,
+    role INTEGER
+);
+
+CREATE TABLE messages
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    room_id INTEGER NOT NULL,
+    content TEXT,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE rooms
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    subject_id INTEGER NOT NULL
+);
+
+CREATE TABLE subjects
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    content TEXT
+);
+
+CREATE TABLE subject_rights
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    subject_id INTEGER NOT NULL
+);
+
