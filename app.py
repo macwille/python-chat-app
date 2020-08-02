7e1c4315ca380cdad1ece582552d56f9bbbc7b98
Ville Manninen <vw.manninen@gmail.com>
2020-08-02 23:59:27 +0300

    test back to gunicorn

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from waitress import serve
 
 
 app = Flask(__name__)
-serve(app, host='0.0.0.0', port=8080, url_scheme='https')
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
